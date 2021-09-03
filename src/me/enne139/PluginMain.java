2d95c5f9ebeff6edb914343395df271a6742a1c8
enne139 <54336269+enne139@users.noreply.github.com>
2021-09-03 23:30:01 +0200

    aggiunta metote savewp

--- src/me/enne139/PluginMain.java
+++ src/me/enne139/PluginMain.java
@@ -2,19 +2,85 @@ package me.enne139;
 
 
 import me.enne139.comandi.Com_savewp;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PluginMain extends JavaPlugin {
 
     @Override
     public void onEnable() {
 
+        //controllo file
+        crea_cartella();
+
         //set eventi
         //getServer().getPluginManager().registerEvents( new Playermovelistener(), this);
 
         //set comandi
-        this.getCommand( "savewp").setExecutor(new Com_savewp());
-        //this.getCommand( "wps").setTabCompleter(new Tab_wps());
+        this.getCommand( "savewp").setExecutor( new Com_savewp() );
+        this.getCommand( "savewp").setTabCompleter( new Com_savewp() );
 
     }
+
+    public void crea_cartella() {
+        // Definiamo il path della cartella da creare
+        String folderName = "plugins/saveCord";
+
+        // Creiamo l'oggetto File
+        File folder = new File(folderName);
+
+        // Verifichiamo che non sia già esistente come cartella
+        if(!folder.isDirectory()) {
+            // In caso non sia già presente, la creiamo
+            folder.mkdir();
+        }
+    }
+
+    public static void scrivi_file(String utente, Location pos, String nome )  {
+        String path = "plugins/saveCord/" + utente;
+        String dati = nome +";"+ pos.getX() +";"+ pos.getY() +";"+ pos.getZ() +";"+ pos.getWorld().getName() + "\n";
+        try {
+            File f1 = new File( path);
+            if(!f1.exists()) {
+                f1.createNewFile();
+            }
+
+            FileWriter fileWritter = new FileWriter( path,true);
+            BufferedWriter bw = new BufferedWriter(fileWritter);
+            bw.append(dati);
+            bw.close();
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static List<String> legge_file(String utente) {
+        String path = "plugins/saveCord/" + utente;
+        List<String> list = new ArrayList<String>();
+        try {
+            File f1 = new File( path);
+            if(!f1.exists()) {
+                f1.createNewFile();
+            }
+
+            FileReader fileWritter = new FileReader( path);
+            BufferedReader bw = new BufferedReader(fileWritter);
+            while ( bw.ready() ) {
+                list.add( bw.readLine() );
+            }
+            bw.close();
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 }
--- src/me/enne139/comandi/Com_savewp.java
+++ src/me/enne139/comandi/Com_savewp.java
@@ -1,21 +1,98 @@
 package me.enne139.comandi;
 
+import me.enne139.PluginMain;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
-public class Com_savewp implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Com_savewp implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if ( commandSender instanceof Player)
         {
-            Player player = (Player) commandSender;
-            player.sendMessage("cioa");
+            Player player = (Player) commandSender;                   // ottine player che ha usato il comando
+            Location pos = player.getLocation();                      // ottine la posizione del player
+            String uuid = new String();
+
+            if ( !( strings.length==1 || strings.length==2 ) ) {      // se non ha 1 o 2 argomenti stampa la sntassi
+                player.sendMessage(ChatColor.YELLOW + "/savewp nome [privato]");
+                return false;
+            }
+
+            if ( strings.length==2 ) {                                // se ci sono 2 argoemnti
+                if ( strings[1].equals("publico") ) {                 // se il secondo argoemnto
+                    uuid = "GLOBAL";
+                } else if ( strings[1].equals("privato") ){
+                    uuid = player.getUniqueId().toString();
+                } else {                                              // se il secondo argomento non è una scelta valida
+                    player.sendMessage(ChatColor.YELLOW + command.toString() + " nome [privato]");
+                    return false;
+                }
+            }
+
+            if ( strings.length==1 ) {                                // senza specifica è privato
+                uuid = player.getUniqueId().toString();
+            }
+
+
+            String nome = strings[0];
+            double x = pos.getX();
+            double y = pos.getY();
+            double z = pos.getZ();
+            World world = pos.getWorld();
+            boolean trovato = false;
+
+            List<String> p = PluginMain.legge_file( uuid);
+            for ( int i=0; i<p.size(); i++) {
+                if ( (p.get(i)).split( ";")[0].equals(nome) ) {
+                    trovato = true;
+                    break;
+                }
+            }
+            if ( trovato ) {
+                player.sendMessage(ChatColor.RED + "waypoin già esistente");
+            } else {
+                PluginMain.scrivi_file( uuid, pos, strings[0]);
+                player.sendMessage(ChatColor.GREEN + "waypoint aggiunto");
+            }
+
         }
 
 
         return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        List<String> arg = new ArrayList<String>();
+
+        if ( commandSender instanceof Player)
+        {
+            Player player = (Player) commandSender;
+
+            if ( strings.length == 1 ) {
+                arg.add( "nome");
+
+            } else if ( strings.length == 2) {
+                arg.add( "privato");
+                arg.add( "publico");
+            }
+
+
+            //player.sendMessage( Integer.toString(strings.length) );
+        }
+
+        return arg;
     }
 }
